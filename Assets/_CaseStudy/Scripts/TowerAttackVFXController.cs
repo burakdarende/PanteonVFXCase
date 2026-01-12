1cf9cf4541f66656dfe43188913cd403c2a3f5ac
burak <darendeburak@gmail.com>
2026-01-12 03:34:52 +0300

    prefab update

--- Assets/_CaseStudy/Scripts/TowerAttackVFXController.cs
+++ Assets/_CaseStudy/Scripts/TowerAttackVFXController.cs
@@ -1,55 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TowerAttackVFXController : MonoBehaviour
 {   
-     [Header("VFX References (assign in Inspector)")]
-    [SerializeField] private ParticleSystem muzzleFlashDirectional;
-    [SerializeField] private ParticleSystem muzzleFlashCore;
-    [SerializeField] private ParticleSystem projectilePs;
+    [Header("VFX Prefab Root")]
+    [SerializeField] private ParticleSystem weaponVFXRoot; 
 
-    [Header("Test")]
+    [Header("Test Controls")]
     [SerializeField] private KeyCode fireKey = KeyCode.Space;
-    [SerializeField] private bool restartWithClear = true;
-
-    private void Awake()
-    {
-        // Don't auto-play at scene start
-        DisablePlayOnAwake(muzzleFlashDirectional);
-        DisablePlayOnAwake(muzzleFlashCore);
-        DisablePlayOnAwake(projectilePs);
-    }
 
     private void Update()
     {
         if (Input.GetKeyDown(fireKey))
+        {
             Fire();
+        }
     }
 
     public void Fire()
     {
-        Trigger(muzzleFlashDirectional);
-        Trigger(muzzleFlashCore);
-        Trigger(projectilePs);
-    }
-
-    private void Trigger(ParticleSystem ps)
-    {
-        if (!ps) return;
-
-        if (restartWithClear)
-            ps.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
-        else
-            ps.Stop(true, ParticleSystemStopBehavior.StopEmitting);
-
-        ps.Play(true);
-    }
-
-    private void DisablePlayOnAwake(ParticleSystem ps)
-    {
-        if (!ps) return;
-        var main = ps.main;
-        main.playOnAwake = false;
+        if (weaponVFXRoot != null)
+        {
+            weaponVFXRoot.Play(true); 
+        }
     }
 }
